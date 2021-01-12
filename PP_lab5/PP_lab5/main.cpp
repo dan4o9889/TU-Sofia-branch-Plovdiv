5f66663aa409c437529f8be8fd33e183b6f4ad86
Daniel Ivanov <d.ivanov9889@gmail.com>
2021-01-12 14:18:01 +0200

    update folder

---
+++ PP_lab5/PP_lab5/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  PP_lab5
+//
+//  Created by Daniel Ivanov on 14.11.20.
+//  Copyright © 2020 Daniel Ivanov. All rights reserved.
+//
+
+#include <iostream>
+#include <libiomp/omp.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
